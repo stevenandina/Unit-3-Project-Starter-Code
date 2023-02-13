a56a4246118504ea82f48fcad95e5fc6910e7418
stevenandina <125302613+stevenandina@users.noreply.github.com>
2023-02-13 12:23:46 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let valiablen1=document.querySelector(".option one");
+let valiable2=document.querySelector(".option two" );
+
+let variable3=document.querySelector(" .option uno");
+let variable4=document.querySelector(".option dos");
+
+let variable5=document.querySelector(".juguete"); 
+let variable6=document.querySelector(".nada" );
+
+let variabl5=document.querySelector(".option-one-end");
+let variabl6=document.querySelector(" .option-two-end");
+
+let variable7=document.querySelector(" .option ai");
+let variable8=document.querySelector(" .option be");
+let screen9=document.querySelector(".option-one-screen");
+let screen10=document.querySelector(".option-two-screen");
+
+
+
+
